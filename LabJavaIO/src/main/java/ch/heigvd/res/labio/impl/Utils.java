9a1e5df1d8eb57b60e815e5a73d2b1dae5e2ea23
Dherleth <erwan.moreira@gmail.com>
2019-03-13 12:58:22 +0100

    Passes the UtilsTest

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -12,15 +12,39 @@ public class Utils {
 
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
+   * the next line in the string passed in arguments.
+   *
    * @param lines a string that may contain 0, 1 or more lines
    * @return an array with 2 elements; the first element is the next line with
    * the line separator, the second element is the remaining text. If the argument does not
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    String windowsReturn = "\r\n";
+    String macOSReturn = "\r";
+    String unixReturn = "\n";
+    String remainingString = "";
+    int endOfLine = 0;
+    String[] response = new String[2];
 
+    // We prepare the default response, when there is no line separator
+    response[0] = "";
+    response[1] = lines;
+
+
+    // Finds which kind of return we have
+    if ((endOfLine = lines.indexOf(windowsReturn)) > 0) {
+      endOfLine += 2;
+    } else if (((endOfLine = lines.indexOf(macOSReturn)) > 0) || ((endOfLine = lines.indexOf(unixReturn)) > 0)) {
+      endOfLine += 1;
+    }
+
+    // We prepare the response isolating the next line
+    if (endOfLine > 0) {
+      response[0] = lines.substring(0, endOfLine);
+      response[1] = lines.substring(endOfLine);
+    }
+
+    return response;
+  }
 }
